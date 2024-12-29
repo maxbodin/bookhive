e5003cf01a5e4529224d52d74219861c4f8e8365
Maxime Bodin <maxime.bodin@mailo.com>
2024-12-30 00:53:20 +0100

    Basic displaying for books.

---
+++ components/books/booksGrid.tsx
@@ -0,0 +1,16 @@
+import { Book } from "@/app/types/book";
+import { BookDetail } from "@/components/books/bookDetail";
+
+interface BooksGridProps {
+  books: Book[];
+}
+
+export async function BooksGrid({
+                       books,
+                     }: BooksGridProps) {
+  return <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+    { books.map((book) => (
+      <BookDetail key={ book.id } book={ book }/>
+    )) }
+  </div>
+}
