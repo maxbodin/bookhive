736f09686f330ff09b4da2860271f24ebec19cda
Maxime Bodin <maxime.bodin@mailo.com>
2025-11-19 01:48:21 +0100

    Adding auth buttons skeleton.

--- components/skeletons/auth-buttons-skeleton.tsx
+++ components/skeletons/auth-buttons-skeleton.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function AuthButtonsSkeleton() {
   return (
     <div className="flex flex-row items-center gap-2">
-      {/* Skeleton for the email input field */}
-      <Skeleton className="h-10 w-[150px]" />
-      {/* Skeleton for the sign-in button */}
-      <Skeleton className="h-10 w-[80px]" />
+      {/* Skeleton for the email input field */ }
+      <Skeleton className="h-10 w-[150px]"/>
+      {/* Skeleton for the sign-in button */ }
+      <Skeleton className="h-10 w-[80px]"/>
     </div>
   );
 }
