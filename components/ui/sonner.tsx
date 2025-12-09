1a061e5bc04072e608e69b26db153ec69c7aa43e
Maxime Bodin <maxime.bodin@mailo.com>
2025-12-09 18:38:43 +0100

    Updating sonner for error state, better ui.

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -21,6 +21,8 @@ const Toaster = ( { ...props }: ToasterProps ) => {
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          error:
+            "group-[.toaster]:text-destructive",
         },
       } }
       { ...props }
